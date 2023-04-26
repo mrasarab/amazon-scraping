89e8af7e2ac94c3e423328ea686dffed1e29a804
mrasarab <120579485+mrasarab@users.noreply.github.com>
2023-04-27 00:08:09 +0200

    Update amazon_scraping.py

--- amazon_scraping.py
+++ amazon_scraping.py
@@ -1,43 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
-import secrets
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
+# Get the URL of the product to monitor from the environment variables
 URL = os.getenv('my_shoes')
 
+# Set the user-agent header to avoid being blocked by the website
 headers = {
-    'user-agent': os.getenv('user-agent')}
+    'user-agent': os.getenv('user-agent')
+}
 
-page = requests.get(URL,headers = headers)
-soup = BeautifulSoup(page.content,'html.parser')
+# Send a GET request to the product page and parse the HTML content
+page = requests.get(URL, headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
 
+# Extract the title and price of the product
 product_price = soup.find(class_='a-price-whole').get_text()
 product_title = soup.find(id='productTitle').get_text()
+
+# Extract the first two digits of the price to check against the threshold
 product_price1 = (product_price)[0:2]
 
-
+# Function to check the product price and send an email if it is below a certain threshold
 def price_check():
-    if int(product_price1)>34 :
+    # Check if the product price is below 34 euros
+    if int(product_price1) > 34:
+        # Set up the email message with the necessary details
         email_sender = os.getenv('email_sender')
         msg = MIMEMultipart()
         msg["subject"] = "schuhe"
         msg["from"] = email_sender
         msg["to"] = email_sender
         msg.attach(MIMEText("the price of shoe is below 34 euro"))
+        
+        # Set up the SMTP server and send the email
         server = smtplib.SMTP(os.getenv('Smtp_server'), 25)
         server.starttls()
-        server.login(os.getenv('Smtp_Username'),os.getenv('Smtp_Password'))
+        server.login(os.getenv('Smtp_Username'), os.getenv('Smtp_Password'))
         text = msg.as_string()
-        server.sendmail(email_sender,email_sender, text)
+        server.sendmail(email_sender, email_sender, text)
         server.quit()
-    
-if __name__ =="__main__":
+
+# Check the product price and send an email if it is below the threshold
+if __name__ == "__main__":
     price_check()
-
-
-
